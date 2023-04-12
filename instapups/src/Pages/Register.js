ca678c7d35954e1c336a66aeee7573d8d8c01366
Shinyn <mattias.alm@hotmail.com>
2023-04-12 03:12:15 +0200

    Added register form

--- instapups/src/Pages/Register.js
+++ instapups/src/Pages/Register.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <input type="text" placeholder="Username" className="register-username" />
+      <input type="password" placeholder="Password" className="register-password" />
+      <input type="password" placeholder="Repeat password" className="register-repeat-password" />
+      <button className="register-button" onClick={goToLogin}>
+        Register
+      </button>
+      <p className="register-error-message"></p>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
+
+function goToLogin() {
+  //TODO: Gör en fetch till backend med POST, lägg till användaren i databasen
+  window.location = "/";
+}
