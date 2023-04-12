ceaf1337baea838f3920efdaf09e510f44cccc2b
Shinyn <mattias.alm@hotmail.com>
2023-04-12 21:57:57 +0200

    Updated login with react components, added fetch test

--- instapups/src/Pages/Login.js
+++ instapups/src/Pages/Login.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [usernameValue, setUsernameValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const onChangeUsername = (event) => {
+    setUsernameValue(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPasswordValue(event.target.value);
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Username" className="login-username" />
-      <input type="password" placeholder="Password" className="login-password" />
+      <input
+        type="text"
+        placeholder="Username"
+        className="login-username"
+        onChange={onChangeUsername}
+        value={usernameValue}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="login-password"
+        onChange={onChangePassword}
+        value={passwordValue}
+      />
       <button className="login-button" onClick={goToProfile}>
         Login
       </button>
@@ -14,7 +37,13 @@ const Login = () => {
 
 export default Login;
 
-function goToProfile() {
-  //TODO: Gör en fetch till backend med POST, om login är korrekt gå till /profile
-  window.location = "/profile";
+async function goToProfile() {
+  // Test for fetching
+  const response = await fetch("https://fakestoreapi.com/products/1");
+  const data = await response.json();
+  console.log(data);
+
+  // TODO: if (response.status === 200) {
+  // window.location = "/profile";
+  // }
 }
