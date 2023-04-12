385e1f915c3a42bdb6e80231e0ffef9dcc0b99c9
Shinyn <mattias.alm@hotmail.com>
2023-04-12 03:11:49 +0200

    Added login form

--- instapups/src/Pages/Login.js
+++ instapups/src/Pages/Login.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <input type="text" placeholder="Username" className="login-username" />
+      <input type="password" placeholder="Password" className="login-password" />
+      <button className="login-button" onClick={goToProfile}>
+        Login
+      </button>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
+
+function goToProfile() {
+  //TODO: Gör en fetch till backend med POST, om login är korrekt gå till /profile
+  window.location = "/profile";
+}
