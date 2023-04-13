fb50c0e88031b041c5249d5d23a3e9d5401b4f3d
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-13 21:25:43 +0200

    fetch the data and map into Posts

--- instapups/src/Pages/Profile.js
+++ instapups/src/Pages/Profile.js
@@ -1,16 +1,33 @@
 import CreatePosts from "../Modules/CreatePosts"
 import Sidemenu from "../Modules/Sidemenu"
 import '../sass/Pages/Profile.pages.scss'
+import { useState, useEffect } from "react"
+import Post from "../Modules/Post"
 
 const Profile = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch('http://localhost:5050/posts')
+      const res = await response.json()
+      setPosts(res)
+    }
+    getPosts()
+  }, [])
+
   return (
     <div className= 'profileWrapper'>
       <Sidemenu />
       <div>
     <CreatePosts />
+    {posts.map((post) => (
+      <Post key={post._id} post={post} />
+    ))}
+    
     </div>
-    <div>
-      
+    <div className='Profile-filler'>
+
     </div>
     </div>
   )
