4f0ef913e0a90e1068140771195a8c40c5402874
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-14 14:24:33 +0200

    updated path

--- instapups/src/Modules/Sidemenu.js
+++ instapups/src/Modules/Sidemenu.js
@@ -4,7 +4,7 @@ import '../sass/Modules/Sidemenu.modules.scss'
 const Sidemenu = () => {
   return (
     <div className='sideMenuWrapper'>
-        <Link to="/"><button>Home</button></Link>
+        <Link to="/home"><button>Home</button></Link>
         <Link to="/profile"><button>Your profile</button></Link>
         <Link to="/members"><button>Members</button></Link>
     </div>
