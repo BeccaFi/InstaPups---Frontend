4501bba92bc1471cfbd573f13d986b4dd1163089
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-14 13:53:47 +0200

    changed how the props are adressed

--- instapups/src/Modules/Post.js
+++ instapups/src/Modules/Post.js
@@ -4,7 +4,7 @@ import { useState} from 'react'
 
 //Måste kolla igenom props.namnen som fetchar sedan
 const Post = (props) => {
-    const [likes, setLikes] = useState(props.likes)
+    const [likes, setLikes] = useState(props.props.likes)
     const [comments, setComments] = useState(false)
     const [bark, setBark] = useState('')
 
@@ -28,23 +28,30 @@ const Post = (props) => {
 
     }
 
-  return (
-    <div>
+    return (
         <div>
-            <img src={props.url} /> <h2>{props.username}</h2>
-        </div>
-        <p>{props.comments}</p>
-        <div><div><img src={bone} />{props.likes}</div><button onClick={(e)=> setComments(true) }>Barks{props.comments.amount}</button></div>
-        {comments && ( <div>
-            {props.comments.map((comment) => { return <div> <img src={props.comments.username}></img> <p>{comment}</p></div> })}
+          <div>
+            <img src={props.props.url} /> <h2>{props.props.username}</h2>
+          </div>
+          <p>{props.props.comments}</p>
+          <div>
             <div>
-                <img src={props.comments.profilepicture}></img>
-                <input type='text' placeholder='Bark here...' onChange={(e)=>setBark(e.target.value)} />
-                <button onClick={(e) => postYourBark()}>Bark</button>
+              <img src={bone} />{props.props.likes}
             </div>
-        </div>)}
-    </div>
-  )
-}
+            <button onClick={(e)=> setComments(true)}>Barks{props.props.comments.amount}</button>
+          </div>
+          {comments && (
+            <div>
+              {props.props.comments.map((comment) => { return <div> <img src={props.props.comments.username}></img> <p>{comment}</p></div> })}
+              <div>
+                <img src={props.props.comments.profilepicture}></img>
+                <input type='text' placeholder='Bark here...' onChange={(e)=>setBark(e.target.value)} />
+                <button onClick={(e) => postYourBark(e)}>Bark</button>
+              </div>
+            </div>
+          )}
+        </div>
+      )
+    }
 
 export default Post
