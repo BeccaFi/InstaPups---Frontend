92e90c9eb9269675fa21bc99073337cd08500aee
Shinyn <mattias.alm@hotmail.com>
2023-04-16 00:53:03 +0200

    Added logout button that logs you out

---
+++ instapups/src/Modules/Logout.jsx
@@ -0,0 +1,16 @@
+const Logout = () => {
+  function logout() {
+    document.cookie = "authToken=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    window.location.reload();
+  }
+
+  return (
+    <>
+      <button id="logout-btn" onClick={logout}>
+        Logout
+      </button>
+    </>
+  );
+};
+
+export default Logout;
