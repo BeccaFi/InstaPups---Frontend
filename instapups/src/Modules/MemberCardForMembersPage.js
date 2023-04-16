d9655ebb763419a8163a25cf01e0b281167215bc
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-16 11:25:20 +0200

    Created a first draft of card for each member

---
+++ instapups/src/Modules/MemberCardForMembersPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//First draft of MemberCardForMembersPage
+// Need to update the parameters to match the data from the database
+// Need to update the onClick function to follow the user
+// Need to update the onClick function to unfollow the user
+const MemberCardForMembersPage = ({username, usersprofilepic, userid }) => {
+  return (
+    <div>
+        <img src={usersprofilepic} alt={username}/>
+        <button onClick={() => console.log(userid)}>Follow</button>
+        <h2>{username}</h2>
+    </div>
+
+  )
+}
+
+export default MemberCardForMembersPage
