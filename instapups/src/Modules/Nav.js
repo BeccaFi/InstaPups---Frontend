9b2b0ec115d7801b0df051ede0adf6b29c5e3647
Shinyn <mattias.alm@hotmail.com>
2023-04-17 05:18:37 +0200

    Imported sass and prettier formated code

--- instapups/src/Modules/Nav.js
+++ instapups/src/Modules/Nav.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../sass/Modules/Nav.modules.scss";
 
 const Nav = () => {
   return (
-    <div>
-        <h1>Nav</h1>
-        <Link to="/home"><button>Home</button></Link>
-        <Link to="/register"><button>Register</button></Link>
-        <Link to="/profile"><button>Profile</button></Link>
-        <Link to="/members"><button>Members</button></Link>
+    <div className="navbar">
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
+      <Link to="/register">
+        <button>Register</button>
+      </Link>
+      <Link to="/profile">
+        <button>Profile</button>
+      </Link>
+      <Link to="/members">
+        <button>Members</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
