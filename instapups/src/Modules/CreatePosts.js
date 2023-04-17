22e2ae389fcb9c8507dab8d6dca1bbd5717c497b
Shinyn <mattias.alm@hotmail.com>
2023-04-17 05:20:04 +0200

    Added className and prettier formated code

--- instapups/src/Modules/CreatePosts.js
+++ instapups/src/Modules/CreatePosts.js
@@ -1,60 +1,66 @@
-import { useState } from "react"
-import addimage from '../Media/Icons/add-image.png'
-import '../sass/Modules/CreatePosts.modules.scss'
+import { useState } from "react";
+import addimage from "../Media/Icons/add-image.png";
+import "../sass/Modules/CreatePosts.modules.scss";
 
 const CreatePosts = () => {
-    const [bark, setBark] = useState('')
-    const [popup, setPopup] = useState(false)
-    const [image, setImage] = useState([])
+  const [bark, setBark] = useState("");
+  const [popup, setPopup] = useState(false);
+  const [image, setImage] = useState([]);
 
-    const postingBarks = async (e) => {
-        e.preventDefault()
-        const text = bark
-        const photos = image
-        var date = new Date();
-        var datePosted = date.toISOString().slice(0, 16).replace('T', ' ');
-        console.log(datePosted)
-        const response = await fetch('http://localhost:5051/posts/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-               text,
-               photos,
-                datePosted
-            }),
-            credentials: 'include'
-        })
-        const res = await response.json()
-        console.log(res)
-        // Här behövs det göras saker
-        
-    }
-
+  const postingBarks = async (e) => {
+    e.preventDefault();
+    const text = bark;
+    const photos = image;
+    var date = new Date();
+    var datePosted = date.toISOString().slice(0, 16).replace("T", " ");
+    console.log(datePosted);
+    const response = await fetch("http://localhost:5051/posts/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text,
+        photos,
+        datePosted,
+      }),
+      credentials: "include",
+    });
+    const res = await response.json();
+    console.log(res);
+    // Här behövs det göras saker
+  };
 
   return (
-    <div className='Your-bark'>
-        <input
-        className='Your-bark-input'
+    <div className="Your-bark">
+      <input
+        className="Your-bark-input"
         onChange={(e) => setBark(e.target.value)}
-        type='text'
-        placeholder='Bark here...'
-        />
-        <div className='Your-bark-icon-Wrapper'>
-            <img className='imageIcon' src= {addimage} alt= 'icon of an image' onClick={(e) => setPopup(true)}></img>
-            <button className='Your-bark-button' onClick={(e) => postingBarks(e)}>Bark</button>
+        type="text"
+        placeholder="Bark here..."
+      />
+      <div className="Your-bark-icon-Wrapper">
+        <img className="imageIcon" src={addimage} alt="icon of an image" onClick={(e) => setPopup(true)}></img>
+        <button className="Your-bark-button" onClick={(e) => postingBarks(e)}>
+          Bark
+        </button>
+      </div>
+
+      {popup && (
+        <div className="Your-bark-popup">
+          <button className="Your-bark-popup-button" onClick={(e) => setPopup(false)}>
+            X
+          </button>
+          <input
+            className="Your-bark-popup-input"
+            type="text"
+            placeholder="Add an image url"
+            onChange={(e) => setImage([e.target.value])}
+          />
         </div>
-
-        {popup && (
-            <div className='Your-bark-popup'>
-                <button onClick={(e) => setPopup(false)}>X</button>
-                <input className='Your-bark-popup-input' type='text' placeholder='Add an image url' onChange={(e) => setImage([e.target.value])} />
-            </div>
-        )}
-        
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CreatePosts
+export default CreatePosts;
