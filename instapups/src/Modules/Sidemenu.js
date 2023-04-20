80458b4cabf76402df2021f74839ccf41d8cf578
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-20 21:27:51 +0200

    Fetching userinfo so Link goes to members own site

--- instapups/src/Modules/Sidemenu.js
+++ instapups/src/Modules/Sidemenu.js
@@ -1,11 +1,37 @@
 import { Link } from "react-router-dom"
 import '../sass/Modules/Sidemenu.modules.scss'
+import { useEffect, useState } from "react";
 
 const Sidemenu = () => {
+  const [userinfo, setUserinfo] = useState([]);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const response = await fetch("http://localhost:5051/auth/navauth", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      const res = await response.json();
+      if (response.status !== 200) {
+        if (response.status === 401) {
+          return (window.location.href = '/');
+        }
+        console.log(res);
+        return;
+      }
+      setUserinfo(res);
+    };
+    checkAuth();
+  }, []);
+
   return (
     <div className='sideMenuWrapper'>
         <Link to="/home"><button>Home</button></Link>
-        <Link to="/profile"><button>Your profile</button></Link>
+        <Link to={`/members/${userinfo._id}`}><button>Your profile</button></Link>
         <Link to="/members"><button>Members</button></Link>
     </div>
   )
