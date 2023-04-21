486037df18a6afffdcc1fff072be5a28f15980bc
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-04-21 08:31:32 +0200

    Updated the comments-length

--- instapups/src/Modules/Post.js
+++ instapups/src/Modules/Post.js
@@ -113,7 +113,7 @@ const Post = ({ username, comments, likes, _id, content}) => {
         <div>
           <img className="likeButton" src={bone} alt="likes" onClick={(e) => likePost(_id)} /> {likes2.length}
         </div>
-        <button className="barkButton" onClick={() => toggleBarkButton()}>Barks ({comments.length})</button>
+        <button className="barkButton" onClick={() => toggleBarkButton()}>Barks ({comments2.length})</button>
       </div>
       {showComments && (
         <div className= "bark-Field-Wrapper">
