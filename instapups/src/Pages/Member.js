367c7e88dc5b0fe718dbe07ddb454b57fb6f9c1b
Shinyn <mattias.alm@hotmail.com>
2023-04-23 05:10:26 +0200

    Prettier code reformatting

--- instapups/src/Pages/Member.js
+++ instapups/src/Pages/Member.js
@@ -1,66 +1,59 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import Post from '../Modules/Post'
-import '../sass/Pages/Member.pages.scss'
-import Sidemenu from '../Modules/Sidemenu'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Post from "../Modules/Post";
+import "../sass/Pages/Member.pages.scss";
+import Sidemenu from "../Modules/Sidemenu";
 
 const Member = () => {
-    const { id } = useParams();
-    const [member, setMember] = useState([])
-    const [posts, setPosts] = useState([])
-    const [loaded, setLoaded] = useState(false)
+  const { id } = useParams();
+  const [member, setMember] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
-        const getMember = async () => {
-            const response = await fetch(`http://localhost:5051/members/:${id}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include',
-            });
+  useEffect(() => {
+    const getMember = async () => {
+      const response = await fetch(`http://localhost:5051/members/:${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
-            const res = await response.json();
-            if (response.status !== 200) {
-                console.log(res);
-                return;
-            }
-            const valuesArray = Object.values(res)
-            const postArray = valuesArray[1]
-            setPosts(postArray);
-            const memberArray = valuesArray[0]
-            setMember(memberArray);
+      const res = await response.json();
+      if (response.status !== 200) {
+        console.log(res);
+        return;
+      }
+      const valuesArray = Object.values(res);
+      const postArray = valuesArray[1];
+      setPosts(postArray);
+      const memberArray = valuesArray[0];
+      setMember(memberArray);
 
-            
-            setLoaded(true)
-        }
-        getMember()
+      setLoaded(true);
+    };
+    getMember();
+  }, [id]);
 
-        
-    }, [id])
-
-    console.log("Members:" + member)
-    console.log(posts)
+  console.log("Members:" + member);
+  console.log(posts);
 
   return (
-    <div className='memberPageWrapper'>
-        <div></div>
-        <Sidemenu />
-      
-      <div className='memberPage'>
-      {loaded ? ( posts.map((post) => (
-          <Post key={post._id} {...post} />
-      ))) : ( "Loading...")}
-      </div>
-      <div className='memberPage-filler'>
-        <div className='memberPage-filler__profile'>
-        <img src= {member.profilePic} alt='profilePicture' />
-        <h1>{member.username}</h1>
-        </div>
-        </div>
-    </div>
-  )
-}
+    <div className="memberPageWrapper">
+      <div></div>
+      <Sidemenu />
 
-export default Member
+      <div className="memberPage">{loaded ? posts.map((post) => <Post key={post._id} {...post} />) : "Loading..."}</div>
+      <div className="memberPage-filler">
+        <div className="memberPage-filler__profile">
+          <img src={member.profilePic} alt="profilePicture" />
+          <h1>{member.username}</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Member;
