1e5c8f3a78c688575932bc25549edc39c8aecd54
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-25 18:36:33 +0200

    Changed profile feed render turnary operator

--- instapups/src/Pages/Member.js
+++ instapups/src/Pages/Member.js
@@ -4,16 +4,25 @@ import { useEffect, useState } from 'react'
 import Post from '../Modules/Post'
 import '../sass/Pages/Member.pages.scss'
 import Sidemenu from '../Modules/Sidemenu'
+import UserCard from '../Modules/OtherUsersProfileCard'
+import LoggedInUserCard from '../Modules/LoggedInUserProfileCard'
 
 const Member = () => {
     const { id } = useParams();
-    const [member, setMember] = useState([])
-    const [posts, setPosts] = useState([])
-    const [loaded, setLoaded] = useState(false)
+    const [member, setMember] = useState('');
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const [posts, setPosts] = useState([]);
+    const [isFollowing, setIsFollowing] = useState(true);
+    const [followMessage, setFollowMessage] = useState();
+    const [gotPosts, setGotPosts] = useState(true);
+    const [postMessage, setPostMessage] = useState();
+    const [loaded, setLoaded] = useState(false);
+
+    console.log(posts);
 
     useEffect(() => {
         const getMember = async () => {
-            const response = await fetch(`http://localhost:5051/members/:${id}`, {
+            const response = await fetch(`http://localhost:5051/members/${id}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -23,44 +32,52 @@ const Member = () => {
 
             const res = await response.json();
             if (response.status !== 200) {
-                console.log(res);
+                console.log(res); // Make a popup with an error message for the user
                 return;
             }
-            const valuesArray = Object.values(res)
-            const postArray = valuesArray[1]
-            setPosts(postArray);
-            const memberArray = valuesArray[0]
-            setMember(memberArray);
+        
+            setPosts(res.posts);
+            setMember(res.user);
+            setLoggedInUser(res.loggedInUser);
+            
+
+            if(res.followMessage){
+                setIsFollowing(false);
+                setFollowMessage(res.followMessage);
+            }
+
+            if(res.postMessage){
+                setGotPosts(false);
+                setPostMessage(res.postMessage);
+                setPosts(res.posts);
+            }
 
             
-            setLoaded(true)
+         
+            setLoaded(true);
+
         }
         getMember()
 
         
     }, [id])
 
-    console.log("Members:" + member)
-    console.log(posts)
+  
+    // console.log(posts)
 
   return (
     <div className='memberPageWrapper'>
         <div></div>
         <Sidemenu />
-      
-      <div className='memberPage'>
-      {loaded ? ( posts.map((post) => (
-          <Post key={post._id} {...post} />
-      ))) : ( "Loading...")}
-      </div>
-      <div className='memberPage-filler'>
-        <div className='memberPage-filler__profile'>
-        <img src= {member.profilePic} alt='profilePicture' />
-        <h1>{member.username}</h1>
+
+        <div className='memberPage'>
+        { member.username === loggedInUser.username ? <LoggedInUserCard loggedInUser={loggedInUser} /> : <UserCard member={member} following={isFollowing} posts={posts} />}
+        { loaded ? (!isFollowing ? <p> {followMessage} </p> : (!gotPosts ? <p> {postMessage} </p> :  ( posts.map((post) => (<Post key={post._id} {...post} />))))) : ( "Loading..." ) }
         </div>
-        </div>
+        <div></div>
+
     </div>
   )
-}
+} 
 
-export default Member
+export default Member 
