f8c2e2554e9fbee182f359a11a0d5aa508d0c2b0
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-26 19:08:06 +0200

    Added id's (for styling) + cred link for gear icon

--- instapups/src/Modules/Footer.js
+++ instapups/src/Modules/Footer.js
@@ -14,15 +14,16 @@ const Footer = () => {
         </ul>
         </div>
 
-        <div>
+        <div id="attributes-div">
             <h3>Attributes</h3>
-            <ul>
+            <ul id="cred-list">
                 <li><a href="https://www.freepik.com/free-vector/cute-pattern-with-dog-faces-bones_22991841.htm#page=3&query=dog%20wallpaper&position=0&from_view=search&track=robertav1_2_sidr">Image by lesyaskripak</a> on Freepik</li>
                 <li><a href="https://www.flaticon.com/free-icons/add-image" title="add image icons">Add image icons created by nawicon - Flaticon</a></li>
                 <li><a href="https://www.flaticon.com/free-icons/bone" title="bone icons">Bone icons created by Freepik - Flaticon</a></li>
                 <li><a href="https://www.flaticon.com/free-icons/pet-bed" title="pet bed icons">Pet bed icons created by Freepik - Flaticon</a></li>
                 <li><a href="https://www.flaticon.com/free-icons/kennel" title="kennel icons">Kennel icons created by Chanut-is-Industries - Flaticon</a></li>
                 <li><a href="https://www.flaticon.com/free-icons/bark" title="bark icons">Bark icons created by HadKicon - Flaticon</a></li>
+                <li><a href="https://www.flaticon.com/free-icons/settings" title="settings icons">Settings icons created by Freepik - Flaticon</a></li>
             
             </ul>
 
