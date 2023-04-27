68dc0f6db64f6c4add4a618e5ecf0b5085671bad
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 07:34:56 +0200

    Renamed popup --> ErrorPopup

---
+++ instapups/src/Modules/ErrorPopup.js
@@ -0,0 +1,17 @@
+
+import "../sass/Modules/ErrorPopup.modules.scss"
+
+const ErrorPopup = ({onClose}) => {
+  return (
+  <>
+    <div className="Popup-background"></div>
+    <div className='Error-Message-Popup'>
+        <button className='close-Error-Message' onClick={onClose}>❌</button>
+        <p>There seems to be an error with the server at the moment, please try again later and contact us if the issue remains</p>
+        <button className="OK-Error-Message-Button" onClick={onClose}>OK</button>
+    </div>
+    </>
+  )
+}
+
+export default ErrorPopup
