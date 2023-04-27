b10d913cd16118d6e83411d2c42a1e7f5fb2b85e
BeccaFi <112644718+BeccaFi@users.noreply.github.com>
2023-04-27 17:41:55 +0200

    Props destructuring, comments removed

--- instapups/src/Modules/OwnProfileCard.js
+++ instapups/src/Modules/OwnProfileCard.js
@@ -1,30 +1,27 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import '../sass/Modules/OwnProfileCard.modules.scss'
 import settings from '../Media/Icons/settings.png'
 import ChangePicInput from "./ChangeProfilePic";
 
-const OwnProfileCard = (props) => {
-
-    const member = props.loggedInUser;
-    // const followCount = member.following.length; //gotta make these work, getting error for some reason
-    // const postCount = props.posts.length;
+const OwnProfileCard = ({loggedInUser, posts}) => {
+    const postCount = posts.length;
     const [changePic, setChangePic] = useState(false);
 
     return (
         <div className='member-card'>
             <div className='member-card-main-info'>
                 <div className="member-card-left">
-                        <img src= {member.profilePic} alt='profilePicture' onClick={() => setChangePic(true)}/>
+                        <img src= {loggedInUser.profilePic} alt='profilePicture' onClick={() => setChangePic(true)}/>
                         <p className="pic-p-tag" onClick={() => setChangePic(true)}>Change picture</p>
                     <button className="settings-btn" onClick={() => alert('Upcoming feature, stay tuned!')}><p>Settings</p><img alt="gear-icon" src={settings}></img> </button>
-                    {changePic ? <ChangePicInput user={member.username} wantChange={changePic} /> : null}
+                    {changePic ? <ChangePicInput user={loggedInUser.username} wantChange={changePic} /> : null}
                 </div>
                 <div className="member-card-middle">
-                    <h1>{member.username}</h1>
+                    <h1>{loggedInUser.username}</h1>
                     <div className="follows-posts-count">
                         <p>Following: </p>
                         <p>•</p>
-                        <p>Posts: </p>
+                        <p>Posts: {postCount} </p>
                     </div>
                 </div>
                 <div className="member-card-right">
